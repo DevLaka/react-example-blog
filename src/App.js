01145f2fc0eeea2f5e21cd0972880c4721ee0bc6
devlaka <lakshanwarunab@gmail.com>
2022-06-16 12:30:11 +0530

    explain state shallow merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "devlaka",
+      name: { firstName: "dev", lastName: "laka" },
+      age: 25,
     };
   }
 
@@ -16,10 +17,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}
+          </p>
+          {/*  1)  React does shallow Merging of state object.
+                  In simpler terms, looking for the property with matching key
+                  and updates it.
+              
+              2)  In order to ensure that the app is running faultlessly, we are
+                  updating the state to same type of values.
+                  Wrong: this.setState({ name : "devlaka" }) // Object ==> String
+                  Correct: this.setState({ name: { firstName: "siri", lastName: "pala" } }) // Object ==> Object
+                  Doing so will break the app becuase we are rendering
+                  this.state.name.firstName but there is no property called firstName
+                  after updateing the state.*/}
           <button
             onClick={() => {
-              this.setState({ name: "Siri" });
+              this.setState({ name: { firstName: "siri", lastName: "pala" } });
             }}
           >
             Change name
