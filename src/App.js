339b4e976eef3ef878f72c7a42cc5822683fdecb
devlaka <lakshanwarunab@gmail.com>
2022-06-21 09:34:15 +0530

    add search

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ class App extends Component {
   // Component Life Cycle Execution Order: 1
   // Initialize the state here.
   constructor() {
+    console.log("Constructor");
     super();
     this.state = {
       articles: [],
+      searchTerm: "",
     };
   }
 
@@ -16,6 +18,7 @@ class App extends Component {
   // Mounting is the first time a component is placed in the DOM.
   // Happens once per component life cycle.
   componentDidMount() {
+    console.log("ComponentDidMount");
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((articles) =>
@@ -36,11 +39,25 @@ class App extends Component {
   // Component Life Cycle Execution Order: 3
   // Re-renders beacuse state is updated in componentDidMount.
   render() {
+    // Another solution is moving filterd array here.
+    // And, then map over it inside return.
+    console.log("Render");
     return (
       <div className="App">
-        {this.state.articles.map((article) => {
-          return <h1 key={article.id}>{article.title}</h1>;
-        })}
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search articles"
+          onChange={(event) => {
+            console.log(event.target.value);
+            this.setState({ searchTerm: event.target.value.toLocaleLowerCase() });
+          }}
+        />
+        {this.state.articles
+          .filter((article) => article.title.toLocaleLowerCase().includes(this.state.searchTerm))
+          .map((article) => {
+            return <h1 key={article.id}>{article.title}</h1>;
+          })}
       </div>
     );
   }
